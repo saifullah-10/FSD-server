933a1f645f8a0cb4ffdef2d6f61835fd6f1298cd
saifullah-10 <saifullah.bus@gmail.com>
2024-07-17 21:22:26 +0600

    update for deploy

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const users = database.collection("users");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     app.get("/users", async (req, res) => {
